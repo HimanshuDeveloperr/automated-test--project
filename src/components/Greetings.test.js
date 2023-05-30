7288fb3b53d8c437da96cea8edd4535003b13dde
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-05-30 22:31:46 +0530

    created test suite

--- src/components/Greetings.test.js
+++ src/components/Greetings.test.js
@@ -1,14 +1,17 @@
 import {render,screen} from "@testing-library/react"
 import Greetings from "./Greetings"
 
-test("renders hello world as a text",()=>{
-    //arrange things that u want to render
-    render(<Greetings/>)
+describe("Greetings Components",()=>{
 
-    // act 
-    // nothing to execute eg:-functions etc...
-
-    // assert
-    const text=screen.getByText("Hello World!")
-    expect(text).toBeInTheDocument()
+    test("renders hello world as a text",()=>{
+        //arrange things that u want to render
+        render(<Greetings/>)
+    
+        // act 
+        // nothing to execute eg:-functions etc...
+    
+        // assert
+        const text=screen.getByText("Hello World!")
+        expect(text).toBeInTheDocument()
+    })
 })
